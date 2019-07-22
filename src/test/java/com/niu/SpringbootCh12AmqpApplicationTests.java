ba7100dd41b393da3962c9570eefdc4d258f2699
18300603955 <oceaniu@163.com>
2019-07-22 20:41:50 +0800

    topicTest

--- src/test/java/com/niu/SpringbootCh12AmqpApplicationTests.java
+++ src/test/java/com/niu/SpringbootCh12AmqpApplicationTests.java
@@ -1,6 +1,7 @@
 package com.niu;
 
 import com.niu.config.RabbitFanoutConfig;
+import com.niu.config.RabbitTopicConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -28,6 +29,23 @@ public class SpringbootCh12AmqpApplicationTests {
 	@Test
 	public void faoutTest(){
 		rabbitTemplate.convertAndSend(RabbitFanoutConfig.FANOUTNAME,null,"hello faout!");
+	}
+
+	@Test
+	public void topicTest(){
+		rabbitTemplate.convertAndSend(RabbitTopicConfig.TOPICNAME,
+				"xiaomi.news","小米新闻...");
+		rabbitTemplate.convertAndSend(RabbitTopicConfig.TOPICNAME,
+				"huawei.news","华为新闻...");
+		rabbitTemplate.convertAndSend(RabbitTopicConfig.TOPICNAME,
+				"xiaomi.phone","小米手机...");
+		rabbitTemplate.convertAndSend(RabbitTopicConfig.TOPICNAME,
+				"huawei.phone","华为手机...");
+		rabbitTemplate.convertAndSend(RabbitTopicConfig.TOPICNAME,
+				"phone.news","手机新闻...");
+
+
+
 	}
 
 }

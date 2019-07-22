5ea38d91b6e8f54a04720692f22091acd9a06b23
18300603955 <oceaniu@163.com>
2019-07-22 20:18:12 +0800

    FaoutReciever

---
+++ src/main/java/com/niu/FaoutReciever.java
@@ -0,0 +1,19 @@
+package com.niu;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by ami on 2019/7/22.
+ */
+@Component
+public class FaoutReciever {
+    @RabbitListener(queues = "queue-one")
+    public void handler1(String message) {
+        System.out.println("FanoutReceiver:handler1:" + message);
+    }
+    @RabbitListener(queues = "queue-two")
+    public void handler2(String message) {
+        System.out.println("FanoutReceiver:handler2:" + message);
+    }
+}
